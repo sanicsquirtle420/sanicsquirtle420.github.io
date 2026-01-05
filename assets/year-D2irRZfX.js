9c3445e7985ce5b18e9332c490f620661f5d5548
Diego Ruiz <dr1116uiz@gmail.com>
2026-01-04 23:53:12 -0600

    Updates

--- assets/year-D2irRZfX.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Cx4PjoKq.js";function o(){const e=()=>`This website is hosted by GitHub Pages - Diego R. (${new Date().getFullYear()})`;return t.jsx("p",{children:e()})}export{o as f};
